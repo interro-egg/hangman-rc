ea80c91a399fe97c34eedce39a6a5fa18c184380
dcard <me@dcard.pt>
2022-12-12 17:48:55 +0000

    run formatter

--- src/common/common.c
+++ src/common/common.c
@@ -2,10 +2,10 @@
 #include <string.h>
 
 int parseEnum(const char *strings[], char *to_parse) {
-    for (unsigned int i = 0; i < sizeof(&strings) / sizeof(char *); i++) {
-        if (strcmp(strings[i], to_parse) == 0) {
-            return (int)i;
-        }
-    };
-    return -1;
+  for (unsigned int i = 0; i < sizeof(&strings) / sizeof(char *); i++) {
+    if (strcmp(strings[i], to_parse) == 0) {
+      return (int)i;
+    }
+  };
+  return -1;
 }
